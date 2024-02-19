2e527d7e79f7a93c6873e25b6f48f627cdd9abd5
Keenan <felipekeenan2000@gmail.com>
2024-02-19 09:46:22 -0300

    rieopkfod

--- my-project/src/components/LastPic.jsx
+++ my-project/src/components/LastPic.jsx
@@ -4,7 +4,9 @@ const LastPic = () => {
     return (
         <section className="bg-[#e8e9ee]  ">
             <div className="container h-[500px] p-20 w-[80%]" style={{ backgroundImage: `url(${bg4})`, backgroundPosition: "center", backgroundSize: "cover" }}></div>
+            <h1>teste</h1>
         </section>
+        
     )
 }
 
